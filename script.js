91ab3fadcfa3f55251bc189d09e6d94ee5546ed4
Jacob <57625094+Snake0good@users.noreply.github.com>
2022-02-09 14:52:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ var page3 = document.getElementById('myModal2')
 
 // Get the button that opens the modal
 var exploreBtn = document.getElementById("explore");
-var selectionBtn = document.querySelector('.single-bottle-container')
-
 
 // When the user clicks the button, open the modal 
 exploreBtn.onclick = function() {
@@ -19,13 +17,6 @@ exploreBtn.onclick = function() {
 var demo = document.getElementById('demo')
 
 
-selectionBtn.onclick = function() {
-  page3.style.display = "block";
-  openingPage.style.display = "none";
-  selectionsPage.style.display = "none";
-}
-
-
 
 // modal 2 or page 3 vars and stuff AND handling 
 var minusBtn = document.getElementById('minus')
@@ -33,15 +24,16 @@ var plusBtn = document.getElementById('plus')
 var numberItems = document.getElementById('number-items')
 var itemsCount = 1;
 
+
+
+// add or subtract the number of bottles
 minusBtn.onclick = function() {
   if (itemsCount === 0) {
     itemsCount = 0;
   } else {
     itemsCount--
   }
-
   numberItems.innerHTML = itemsCount
-  
 }
 
 plusBtn.onclick = function() {
@@ -50,14 +42,11 @@ plusBtn.onclick = function() {
   } else {
     itemsCount++
   }
-  
   numberItems.innerHTML = itemsCount
 }
 
 
-
-
-
+// going back to page2 from descriptions page
 var backToPageTwoBtn = document.getElementById('backToPage2')
 
 backToPageTwoBtn.onclick = function() {
@@ -67,4 +56,49 @@ backToPageTwoBtn.onclick = function() {
 
   selectionsPage.classList.remove('modal-content-right-slide')
   selectionsPage.classList.add('modal-content-left-slide')
+}
+
+
+
+// --- selecting each individual bottle --- //
+const items = document.querySelectorAll('.single-bottle-container')
+
+// select where the content will go
+const alcContent = document.getElementById('alc-content');
+const volumeContent = document.getElementById('volume-content');
+const acidContent = document.getElementById('acid-content');
+const pHContent = document.getElementById('pH-content');
+const description = document.getElementById('description-content');
+const bottlePrice = document.getElementById('price-content');
+const bottleName = document.getElementById('bottleName');
+const bottleImg = document.getElementById('bottle-image');
+
+
+// cycle through clicks on each item 
+for (let i=0; i<items.length; i++) {
+  items[i].addEventListener('click', function() {
+    var newAlc = items[i].querySelector('.alcohol').innerHTML;
+    var newVol = items[i].querySelector('.volume').innerHTML;
+    var newAcid = items[i].querySelector('.acidity').innerHTML;
+    var newPH = items[i].querySelector('.pH').innerHTML;
+    var newDesc = items[i].querySelector('.describe').innerHTML;
+    var newPrice = items[i].querySelector('.price').innerHTML;
+    var newName = items[i].querySelector('.bottle-name').innerHTML;
+    var newImg = items[i].querySelector('img').src;
+
+    // place the info in the correct spot 
+    alcContent.innerHTML = newAlc;
+    volumeContent.innerHTML = newVol;
+    acidContent.innerHTML = newAcid;
+    pHContent.innerHTML = newPH;
+    description.innerHTML = newDesc;
+    bottlePrice.innerHTML = newPrice;
+    bottleName.innerHTML = newName;
+    bottleImg.src = newImg;
+
+    // show the pages on the click
+    page3.style.display = "block";
+    openingPage.style.display = "none";
+    selectionsPage.style.display = "none";
+  })
 }
